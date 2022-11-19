2a8ada911eb89ad7408f57cd4c77d86196b040ac
JherimyBernas <jherimys.bernas@gmail.com>
2022-11-19 19:04:29 +0800

    Tkinter

--- Python Project.py
+++ Python Project.py
@@ -1,1 +1,11 @@
-print("hi")
+from tkinter import *
+from functools import partial
+
+print("\n***** PROGRAMMED BY *****")
+print("*** JHERIMY S. BERNAS ***")
+
+final1 = Tk()
+final1.title("Cash Register")
+final1.geometry("275x320")
+
+
