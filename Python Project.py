65912d256508ea656d223465944833199deef050
JherimyBernas <jherimys.bernas@gmail.com>
2022-11-19 19:56:00 +0800

    TextVariable

--- Python Project.py
+++ Python Project.py
@@ -10,9 +10,14 @@ final1.title("Cash Register")
 final1.geometry("275x320")
 
 
+expression = ""
+main_str = StringVar()
+subtotal_str = StringVar()
+tax_str = StringVar()
+total_str = StringVar()
 
 dollar_sign = Label(final1, text="$").place(x=30, y=20)
-main_box = Label(final1, width=30, borderwidth=3, relief="sunken", anchor="w").place(x=40, y=20)
+main_box = Label(final1, width=30, textvariable=main_str, borderwidth=3, relief="sunken", anchor="w").place(x=40, y=20)
 
 button1 = Button(final1, text="1", width=3, borderwidth=3).place(x=50, y=50)
 button2 = Button(final1, text="2", width=3, borderwidth=3).place(x=83, y=50)
@@ -35,11 +40,11 @@ subtotal_label = Label(final1, text="Subtotal:", anchor="w").place(x=30, y=185)
 tax_label = Label(final1, text="Tax:", anchor="w").place(x=30, y=225)
 total_label = Label(final1, text="Total:", anchor="w").place(x=30, y=265)
 
-subtotal = Label(final1, width=18, borderwidth=2, relief="sunken", anchor="e")\
+subtotal = Label(final1, textvariable=subtotal_str, width=18, borderwidth=2, relief="sunken", anchor="e")\
     .place(x=123, y=185)
-tax = Label(final1, width=18, borderwidth=2, relief="sunken", anchor="e")\
+tax = Label(final1, textvariable=tax_str, width=18, borderwidth=2, relief="sunken", anchor="e")\
     .place(x=123, y=225)
-total = Label(final1, width=18, borderwidth=2, relief="sunken", anchor="e")\
+total = Label(final1, textvariable=total_str, width=18, borderwidth=2, relief="sunken", anchor="e")\
     .place(x=123, y=265)
 
 final1.mainloop()
